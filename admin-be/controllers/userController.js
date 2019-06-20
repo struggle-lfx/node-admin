aa34b120c1ce4d251c845dc70e34c91493c6e36d
未来可期 <1163220839@qq.com>
2019-06-20 16:57:42 +0800

    修改

--- admin-be/controllers/userController.js
+++ admin-be/controllers/userController.js
@@ -1,8 +1,10 @@
 const userModule = require('../modules/userModule')
 const userController ={
+
+
+
     async signUp(req,res,next){
         res.set('Content-Type','application/json;charset=utf-8');//设置响应格式
-
         let result = await userModule._save(req.body)
         if(result){
             res.render('succ',{    //使用ejs模板，render方法回自动去找目录
